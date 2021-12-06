e47b88473499cddad21a3e3764acc2195fbffebb
Olanrewaju Ibidokun <lanre.bolaji007@gmail.com>
2021-12-06 03:10:46 +0100

    Morning Upadate

--- src/main/java/com/spearhead/nova/model/AuditLogs.java
+++ src/main/java/com/spearhead/nova/model/AuditLogs.java
@@ -3,23 +3,45 @@ package com.spearhead.nova.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+//import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 @Entity
 public class AuditLogs extends DateAudit {
 	
 	private static final long serialVersionUID = 1L;
+
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(generator = "audit_user_generator")
+    @SequenceGenerator(
+   		name = "audit_user_generator",
+   		sequenceName = "audit_user_sequence",
+   		initialValue = 100
+   		)
 	private Long autologId;
-	@Column(nullable = false)
+
+
+	
 	private Integer user_id;
-	@Column(columnDefinition = "JSON", nullable = false)
+	
+	@Column(name="action", length=512, nullable = false)
 	private String action;
 	
 	private String status;
+	
+	public AuditLogs() {
+		super();
+	}
+	
+	public AuditLogs(Long autologId, Integer user_id, String action, String status) {
+		super();
+	
+		this.user_id = user_id;
+		this.action = action;
+		this.status = status;
+	}
 	
 	public Long getAutologId() {
 		return autologId;
@@ -46,9 +68,6 @@ public class AuditLogs extends DateAudit {
 		this.status = status;
 	}
 	
-	public AuditLogs() {
-		super();
-	}
 
 	@Override
 	public String toString() {
